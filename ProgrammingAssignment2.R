8d1c30f7a7b53e4fecd30cd6b40544bfc34c6c9b
ElisaMtz <42253021+ElisaMtz@users.noreply.github.com>
2018-08-15 11:33:43 -0500

    Update ProgrammingAssignment2.R

--- ProgrammingAssignment2.R
+++ ProgrammingAssignment2.R
@@ -1,57 +1,34 @@
-#' Create a special "matrix" object that can cache its inverse.
-#' 
-#' @param x A matrix
-#' 
-#' @return A list containing four functions to set and get the value of the
-#'     matrix and to set and get the inverse of the matrix
-
-
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
-  # Define function to set the value of the matrix. It also clears the old
-  # inverse from the cache
-  
+ 
   set <- function(y) {
     x <<- y    # Set the value
     m <<- NULL # Clear the cache
   }
   
-  # Define function to get the value of the matrix
   get <- function() x
-  
-  # Define function to set the inverse. This is only used by getinverse() when
-  # there is no cached inverse
   
   setInverse <- function(inverse) m <<- inverse
   
-  # Define function to get the inverse
   getInverse <- function() m
-  
-  # Return a list with the above four functions
+
   list(set = set, get = get,
        setInverse = setInverse,
        getInverse = getInverse)
 }
 
 
-
-#' @param x a special matrix created with makeCacheMatrix
-#' 
-#' @return The inverse of the matrix x
-#' 
-
 cacheSolve <- function(x) {
-  m <- x$getInverse() # This fetches the cached value for the inverse
+  m <- x$getInverse() 
   
-  if(!is.null(m)) { # If the cache was not empty, we can just return it
+  if(!is.null(m)) { 
     message("getting cached data")
     return(m)
   }
   
-  # The cache was empty. We need to calculate it, cache it, and then return it.
-  data <- x$get()  # Get value of matrix
-  m <- solve(data) # Calculate inverse
-  x$setInverse(m)  # Cache the result
-  m                # Return the inverse
+  data <- x$get()  
+  m <- solve(data) 
+  x$setInverse(m)  
+  m                
   
 }
